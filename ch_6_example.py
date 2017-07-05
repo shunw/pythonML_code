16a5f04a19610bdaa6b369d34b5a0840605b3502
Li <shunwen.li@hp.com>
2017-07-05 16:38:38 +0800

    update on the ch-6

--- ch_6_example.py
+++ ch_6_example.py
@@ -1,15 +1,18 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.cross_validation import cross_val_score
 from sklearn.cross_validation import train_test_split
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.decomposition import PCA
+from sklearn.learning_curve import learning_curve
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 
 def data_deal():
-    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data', header=None)
+    df = pd.read_csv('wdbc.data', header=None)
 
     X = df.loc[:, 2:].values
     y = df.loc[:, 1].values
@@ -39,7 +42,29 @@ def data_deal():
         scores.append(score)
         # print ('Fold: {n}, Class dist.: {qty}, Acc: {score:.3f}'.format(n = k+1, qty = np.bincount(y_train[train]), score = score))
     
+    scores = cross_val_score(estimator = pipe_lr, X = X_train, y = y_train, cv = 10, n_jobs = 1)
+    # print ('CV accruracy scores: {scores}'.format(scores = scores))
+    # print ('CV accuracy: {score_mean:.3f} +/- {score_std:.3f}'.format(score_mean = np.mean(scores), score_std = np.std(scores)))
+
+    '''diagnosing bias and variace with learning curves'''
+    pipe_lr = Pipeline([('scl', StandardScaler()), ('clf', LogisticRegression(penalty = 'l2', random_state = 0))])
+    train_sizes, train_scores, test_scores = learning_curve(estimator = pipe_lr, X = X_train, y = y_train, train_sizes = np.linspace(.1, 1.0, 10), cv = 10, n_jobs = 1)
+    
+    train_mean = np.mean(train_scores, axis = 1)
+    train_std = np.std(train_scores, axis = 1)
+    test_mean = np.mean(test_scores, axis = 1)
+    test_std = np.std(test_scores, axis = 1)
+    plt.plot(train_sizes, train_mean, color = 'blue', marker = 'o', markersize = 5, label = 'training accuracy')
+    plt.fill_between(train_sizes, train_mean + train_std, train_mean - train_std, alpha = .15, color = 'blue')
+    plt.plot(train_sizes, test_mean, color = 'green', linestyle = '--', marker = 's', markersize = 5, label = 'validation accuracy')
+    plt.fill_between(train_sizes, test_mean + test_std, test_mean - test_std, alpha = .15, color = 'green')
+    plt.grid()
+    plt.xlabel('Number of training samples')
+    plt.ylabel('Accuracy')
+    plt.legend(loc = 'lower right')
+    plt.ylim([.8, 1.0])
+    plt.show()
 
 if __name__ == '__main__':
     data_deal()
-    # stopped at page 203
+    # stopped at page 183/ 208
