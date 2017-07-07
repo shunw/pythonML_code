94f86e63c6e8d931f9ee35cb8cf39e3c3535d6bc
Li <shunwen.li@hp.com>
2017-07-07 16:25:33 +0800

    continue on the ch 6, end with the ROC plot

--- ch_6_example.py
+++ ch_6_example.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy import interp
 from sklearn.cross_validation import cross_val_score
 from sklearn.cross_validation import train_test_split
 from sklearn.cross_validation import StratifiedKFold
@@ -9,10 +10,16 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.learning_curve import learning_curve
 from sklearn.learning_curve import validation_curve
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import make_scorer, f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score, f1_score
+from sklearn.metrics import roc_curve, auc
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 def data_deal():
     df = pd.read_csv('wdbc.data', header=None)
@@ -99,7 +106,76 @@ def data_deal():
 
     clf = gs.best_estimator_
     clf.fit(X_train, y_train)
-    print ('Test accuracy: {score:.3f}'.format(score = clf.score(X_test, y_test)))
+    # print ('Test accuracy: {score:.3f}'.format(score = clf.score(X_test, y_test)))
+
+    '''algorithm selection w/ nested cv'''
+    gs = GridSearchCV(estimator = pipe_svc, param_grid = param_grid, scoring = 'accuracy', cv = 2, n_jobs = -1)
+    scores = cross_val_score(gs, X_train, y_train, scoring = 'accuracy', cv = 5)
+    # print ('CV accuracy: {s_mean:.3f} +/- {s_std:.3f}'.format(s_mean = np.mean(scores), s_std = np.std(scores)))
+
+    
+    gs = GridSearchCV(estimator = DecisionTreeClassifier(random_state = 0), param_grid = [{'max_depth': [1, 2, 3, 4, 5, 6, 7, None]}], scoring = 'accuracy', cv = 5)
+    scores = cross_val_score(gs, X_train, y_train, scoring = 'accuracy', cv = 2)
+    # print ('CV accuracy: {s_mean:.3f} +/- {s_std:.3f}'.format(s_mean = np.mean(scores), s_std = np.std(scores)))
+
+    '''different performance evaluation metrics'''
+    ''''confusion matrix'''
+    pipe_svc.fit(X_train, y_train)
+    y_pred = pipe_svc.predict(X_test)
+    confmat = confusion_matrix(y_true = y_test, y_pred = y_pred)
+    # print (confmat)
+
+    # fig, ax = plt.subplots(figsize = (2.5, 2.5))
+    # ax.matshow(confmat, cmap = plt.cm.Blues, alpha = .3)
+    # for i in range(confmat.shape[0]): 
+    #     for j in range(confmat.shape[1]):
+    #         ax.text(x = j, y = i, s = confmat[i, j], va = 'center', ha = 'center')
+    # plt.xlabel('predicted label')
+    # plt.ylabel('true label')
+    # plt.show()
+
+    '''optimize the precision/ recall of a classification model'''
+    # print ('Precision: {pre:.3f}'.format(pre = precision_score(y_true = y_test, y_pred = y_pred)))
+
+    # print ('Recall: {recall:.3f}'.format(recall = recall_score(y_true = y_test, y_pred = y_pred)))
+
+    # print ('F1: {f1:.3f}'.format(f1 = f1_score(y_true = y_test, y_pred = y_pred)))
+
+    scorer = make_scorer(f1_score, pos_label = 0)
+
+    gs = GridSearchCV(estimator = pipe_svc, param_grid = param_grid, scoring = scorer, cv = 10)
+
+    '''plot a ROC (receiver operating characteristic)'''
+    pipe_lr = Pipeline([('scl', StandardScaler()), ('pca', PCA(n_components = 2)), ('clf', LogisticRegression(penalty = 'l2', random_state = 0, C = 100.0))])
+    X_train2 = X_train[:, [4, 14]]
+    cv = StratifiedKFold(y_train, n_folds = 3, random_state = 1)
+    fig = plt.figure(figsize = (7, 5))
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+    all_tpr = []
+
+    for i, (train, test) in enumerate(cv):
+        probas = pipe_lr.fit(X_train2[train], y_train[train]).predict_proba(X_train2[test]) 
+        fpr, tpr, thresholds = roc_curve(y_train[test], probas[:, 1], pos_label = 1)
+        mean_tpr += interp(mean_fpr, fpr, tpr)
+        mean_tpr[0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, lw = 1, label = 'ROC fold {n} (area = {auc:.2f})'.format(n = i + 1, auc = roc_auc))
+
+    plt.plot([0, 1], [0, 1], linestyle = '--', color = (.6, .6, .6), label = 'random guessing')
+    mean_tpr /= len(cv)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    plt.plot(mean_fpr, mean_tpr, 'k--', label = 'mean ROC (area = {auc_area:.2f})'.format(auc_area = mean_auc), lw = 2)
+    plt.plot([0, 0, 1], [0, 1, 1], lw = 2, linestyle = ':', color = 'black', label = 'perfect performance')
+    plt.xlim([-.05, 1.05])
+    plt.ylim([-.05, 1.05])
+    plt.xlabel('false positive rate')
+    plt.ylabel('true positive rate')
+    plt.title('Receiver Operator Characteristic')
+    plt.legend(loc = 'lower right')
+    plt.show()
+
 
 if __name__ == '__main__':
     data_deal()
