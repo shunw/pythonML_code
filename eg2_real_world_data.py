02a763876eac22e834eec4fca28b8dc648e60180
shunw <shunw.li@outlook.com>
2017-10-29 20:15:49 +0800

    not sure what changed in the eg2

--- eg2_real_world_data.py
+++ eg2_real_world_data.py
@@ -1,9 +1,10 @@
 from collections import Counter
 
 from imblearn.datasets import make_imbalance
-from imblearn.under_sampling import NearMiss
-from imblearn.pipeline import make_pipeline
 from imblearn.metrics import classification_report_imbalanced
+from imblearn.pipeline import make_pipeline
+from imblearn.under_sampling import ClusterCentroids
+from imblearn.under_sampling import NearMiss
 
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
@@ -11,11 +12,24 @@ from matplotlib.colors import ListedColormap
 import numpy as np
 
 from sklearn.datasets import load_iris
-from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
 
+def scatter_plot_2d(x_ls, y_ls):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y_ls))])
+    
+    # plot class samples
+    for idx, c1 in enumerate(np.unique(y_ls)):
+        plt.scatter(x = x_ls[y_ls == c1, 0], y = x_ls[y_ls == c1, 1], 
+                    alpha = .8, c = cmap(idx), 
+                    marker = markers[idx], label = c1)
+    
+    # plt.show()
 
-def scatter_plot(x_ls, y_ls, classifier, resolution = .02):
+def deci_bdry_plot_2d(x_ls, y_ls, classifier, resolution = .02):
     # setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
@@ -55,12 +69,34 @@ def multi_class_under_sampling():
     # print ('Training target statistics: {}'.format(Counter(y_train)))
     # print ('Testing target statistics: {}'.format(Counter(y_test)))
 
-    pipeline = make_pipeline(NearMiss(version = 2, random_state = RANDOM_STATE), LinearSVC(random_state = RANDOM_STATE))
-    pipeline.fit(X_train, y_train)
+    nm = NearMiss(version = 1, random_state = RANDOM_STATE)
+    X_resample_nm, y_resample_nm = nm.fit_sample(X_train, y_train)
 
-    scatter_plot(X[:, [1, 2]], y, pipeline)
-    # print (classification_report_imbalanced(y_test, pipeline.predict(X_test)))
+    cc = ClusterCentroids(random_state = 0)
+    X_resample_cc, y_resample_cc = cc.fit_sample(X_train, y_train)
+    
+    '''plot two in one frame'''
+    fig, (ax0, ax1) = plt.subplots(ncols = 2)
+    # ax0, ax1 = axes.flatten()
 
+    ax0 = scatter_plot_2d(X_resample_nm, y_resample_nm)
+    ax1 = scatter_plot_2d(X_resample_nm, y_resample_nm)
 
+    # fig.tight_layout()
+    plt.show()
+    
+    # pipeline_nm = make_pipeline(NearMiss(version = 1, random_state = RANDOM_STATE), LinearSVC(random_state = RANDOM_STATE))
+    # pipeline_nm.fit(X_train, y_train)
+
+    # pipeline_cc = make_pipeline(ClusterCentroids(random_state = 0), LinearSVC(random_state = RANDOM_STATE))
+    # pipeline_cc.fit(X_train, y_train)
+
+    # print (classification_report_imbalanced(y_test, pipeline_nm.predict(X_test)))
+    # deci_bdry_plot_2d(X[:, [1, 2]], y, pipeline_nm)
+
+    # print (classification_report_imbalanced(y_test, pipeline_cc.predict(X_test)))
+    # deci_bdry_plot_2d(X[:, [1, 2]], y, pipeline_cc)
+    
+    
 if __name__ == '__main__':
     multi_class_under_sampling()
