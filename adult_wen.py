f49ced0d79d479b20e74b6caf7fc7129e0924ecb
shunw <shunw.li@outlook.com>
2017-11-01 21:51:41 +0800

    keep the data cleaning

--- adult_wen.py
+++ adult_wen.py
@@ -1,7 +1,57 @@
+import numpy as np
 import pandas as pd
+import re
 
-df_adult = pd.read_csv('adult.data', header = None)
-df_adult.columns = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'target']
+def check_qm(df, df_col_ls):
+    '''
+    CHECK the question mark number
+    ==============================
+    df_col_ls: list/ col name list
+    df: dataframe to be checked
+    '''
+    for n in df_col_ls:
+        if df[n].dtype == 'int64': continue
+        
+        qestmark_qty = sum([i for i in list(df[n].str.find("?")) if i != -1])
+        if qestmark_qty == 0: continue
+        print ('column name is {name}'.format(name = n))
+        print ('question mark qty is {qty}'.format(qty = qestmark_qty))
 
-# print (df_adult.iloc[27, 1].strip() == '?')
-print (df_adult['workclass'].where(df_adult['workclass'].strip() == '?'))
+def replace_qm(df, df_col_ls):
+    '''
+    REPLACE the question mark with np.nan
+    =====================================
+    df_col_ls: list/ col name list
+    df: dataframe to be used to replace the question mark
+    '''        
+    for n in df_col_ls:
+        if df[n].dtype == 'int64': continue
+        df[n] = df[n].replace(' ?', np.NaN)
+    return df
+
+def make_class_map(df, df_col_ls):
+    '''
+    MAKE class map for each str columns
+    =====================================
+    df_col_ls: list/ col name list
+    df: dataframe to be used to replae the question mark
+    cls_map_dict: dict/ connect column name with the class mapping
+    '''
+    cls_map_dict = dict()
+    for n in df_col_ls:
+        if df[n].dtype == 'int64': continue
+        cls_map_dict[n] = {label:idx for idx,label in enumerate(np.unique(df[n]))}
+    return cls_map_dict
+
+
+
+if __name__ == '__main__':
+    df_adult = pd.read_csv('adult.data', header = None)
+    name_col = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'target']
+    df_adult.columns = name_col
+    # print (df_adult.iloc[26:27, ])
+    # check_qm(df_adult, name_col)
+    
+    df_adult = replace_qm(df_adult, name_col)
+    a = make_class_map(df_adult, name_col)
+    print (a)
