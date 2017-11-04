7faaf8a4c12ac0da6cf45c4f30fede03aefe6a99
shunw <shunw.li@outlook.com>
2017-11-04 14:11:01 +0800

    complete the data preprocess

--- adult_wen.py
+++ adult_wen.py
@@ -26,7 +26,8 @@ def replace_qm(df, df_col_ls):
     '''        
     for n in df_col_ls:
         if df[n].dtype == 'int64': continue
-        df[n] = df[n].replace(' ?', np.NaN)
+        df[n] = df[n].str.strip()
+        df[n] = df[n].replace('?', 'N/A')
     return df
 
 def make_class_map(df, df_col_ls):
@@ -40,14 +41,25 @@ def make_class_map(df, df_col_ls):
     cls_map_dict = dict()
     for n in df_col_ls:
         if df[n].dtype == 'int64': continue
-        print (df[n])
-        print (np.unique(df[n]))
-        cls_map_dict[n] = {label:idx for idx,label in enumerate(np.unique(df[n]))}
+        # print (df[n])
+        # print (np.unique(df[n]))
+        temp_dict = dict()
+        for idx,label in enumerate(np.unique(df[n])): 
+            if label != 'N/A': temp_dict[label] = idx
+            else: temp_dict[label] = -1
+        cls_map_dict[n] = temp_dict
 
-        print (cls_map_dict)
+        # print (cls_map_dict)
     return cls_map_dict
 
-
+def do_class_map(df, df_col_ls, cls_map_dict):
+    '''
+    MAP the category into int with class map
+    '''
+    for n in df_col_ls:
+        if df[n].dtype == 'int64': continue
+        df[n] = df[n].map(cls_map_dict[n])
+    return df
 
 if __name__ == '__main__':
     df_adult = pd.read_csv('adult.data', header = None)
@@ -56,13 +68,18 @@ if __name__ == '__main__':
     # print (df_adult.iloc[26:27, ])
     # check_qm(df_adult, name_col)
     
+    '''
+    DATA PREPROCESS
+    ====================
+    1. strip the str; replace the '?' with 'N/A'
+    2. make the class_map
+    3. replace the str/category into number. 
+    '''
     df_adult = replace_qm(df_adult, name_col)
-    # a = make_class_map(df_adult, name_col)
-    # print (a)
+    cls_map_dict = make_class_map(df_adult, name_col)
+    df_adult = do_class_map(df_adult, name_col, cls_map_dict)
+    
 
-    x = np.array(['b', 'a'], dtype=np.object)
-    # print (np.unique(x))
-    print (x)
 
     '''
     QUESTION: 
